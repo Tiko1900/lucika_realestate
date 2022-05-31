88ce8150a5b23b0b357bc6c21e4ecbba8f0a951d
Lucika <lucika_jeanbaptiste@yahoo.fr>
2022-05-31 06:50:54 -0400

    first commit

--- components/SearchFilters.jsx
+++ components/SearchFilters.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import { Flex, Select, Box, Text, Input, Spinner, Icon, Button } from '@chakra-ui/react';
 import { useRouter } from 'next/router';
 import { MdCancel } from 'react-icons/md';
-import Image from 'next/Image';
+import Image from 'next/image';
 
 import { filterData, getFilterValues } from '../utils/filterData'; 
 
